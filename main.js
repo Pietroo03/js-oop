8dbcbaef53f2236216aaedea417a1e421d77a7ff
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-22 14:16:13 +0200

    add constructor

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ class Veicolo {
     porte
     carburante
 
+    constructor(marca, anno, colore, porte, carburante) {
+        this.marca = marca
+        this.anno = anno
+        this.colore = colore
+        this.porte = porte
+        this.carburante = carburante
+    }
+
 }
 
 
